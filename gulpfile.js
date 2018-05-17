10d19eee7665abae3740188fe29fc833c5587a1e
Aleksandr Siryi <theobroma222@gmail.com>
2018-05-17 04:54:27 +0300

    refactor fonts and images

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var path = require('path');
 var fs = require('fs');
 var rimraf = require('rimraf');
 var del = require('del');
+var cache = require('gulp-cached');
 var runSequence = require('run-sequence');
 var autoprefixer = require('autoprefixer');
 var mqpucker = require('css-mqpacker');
@@ -91,50 +92,68 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest(paths.js));
 });
 
-// Сборка шрифтов
-gulp.task('fonts', function() {
+// Convert ttf->woff, else just copy woff
+gulp.task('ttf2woff', function() {
   return gulp
-    .src(paths.fonts_src + '*.ttf')
+    .src(paths.fonts_src + '*.{ttf,woff}')
     .pipe($.plumber())
     .pipe($.ttf2woff())
     .pipe(gulp.dest(paths.fonts_dest));
 });
-
-gulp.task('images', function() {
-  return gulp
-    .src('src/*.{jpg,png}')
-    .pipe(
-      $.responsive(
-        {
-          '*.jpg': {
-            // Resize all JPG images to 200 pixels wide
-            width: 200
-          },
-          '*.png': {
-            // Resize all PNG images to 50% of original pixels wide
-            width: '50%'
-          },
-          // Resize all images to 100 pixels wide and add suffix -thumbnail
-          '*': {
-            width: 100,
-            rename: { suffix: '-thumbnail' }
-          }
-        },
-        {
-          // Global configuration for all images
-          // The output quality for JPEG, WebP and TIFF output formats
-          quality: 70,
-          // Use progressive (interlace) scan for JPEG and PNG output
-          progressive: true,
-          // Zlib compression level of PNG output format
-          compressionLevel: 6,
-          // Strip all metadata
-          withMetadata: false
-        }
-      )
-    )
-    .pipe(gulp.dest('dist'));
+// Copy font-awsome from node_modules
+gulp.task('copy-fa', function() {
+  return gulp.src('node_modules/font-awesome/fonts/*.woff').pipe(gulp.dest(paths.fonts_dest));
 });
+
+// Сжатие картинок
+gulp.task('imagemin', function() {
+  gulp
+    .src(paths.img + '*.{png,jpg,gif,svg}')
+    .pipe(cache('img'))
+    .pipe(
+      $.imagemin({
+        verbose: true
+      })
+    )
+    .pipe(gulp.dest(paths.bundles));
+});
+
+//responsive
+// gulp.task('images', function() {
+//   return gulp
+//     .src('src/*.{jpg,png}')
+//     .pipe(
+//       $.responsive(
+//         {
+//           '*.jpg': {
+//             // Resize all JPG images to 200 pixels wide
+//             width: 200
+//           },
+//           '*.png': {
+//             // Resize all PNG images to 50% of original pixels wide
+//             width: '50%'
+//           },
+//           // Resize all images to 100 pixels wide and add suffix -thumbnail
+//           '*': {
+//             width: 100,
+//             rename: { suffix: '-thumbnail' }
+//           }
+//         },
+//         {
+//           // Global configuration for all images
+//           // The output quality for JPEG, WebP and TIFF output formats
+//           quality: 70,
+//           // Use progressive (interlace) scan for JPEG and PNG output
+//           progressive: true,
+//           // Zlib compression level of PNG output format
+//           compressionLevel: 6,
+//           // Strip all metadata
+//           withMetadata: false
+//         }
+//       )
+//     )
+//     .pipe(gulp.dest('dist'));
+// });
 
 //Перезагрузка страницы
 gulp.task('browser-sync', function() {
@@ -162,21 +181,24 @@ gulp.task('cssLint', function() {
 
 gulp.task('watch', function() {
   gulp.watch(paths.templates + '**/*.pug', ['pug']);
-  gulp.watch(paths.fonts_src + '**/*.ttf', ['fonts']);
+  gulp.watch(paths.fonts_src + '**/*.{ttf,woff}', ['fonts']);
   gulp.watch(paths.styles + '**/*.scss', ['sass']);
   gulp.watch(paths.scripts + '**/*.js', ['scripts']);
   gulp.watch(paths.symbols + '*.svg', ['symbols']);
-  gulp.watch(paths.img + '*.{png,jpg,gif,svg}', ['img']).on('change', function(event) {
+  gulp.watch(paths.img + '*.{png,jpg,gif,svg}', ['imagemin']).on('change', function(event) {
     if (event.type === 'deleted') {
       del(paths.bundles + path.basename(event.path));
-      delete cache.caches['img'][event.path];
+      delete cache.caches['imagemin'][event.path];
     }
   });
 });
 
-gulp.task('compile', ['pug', 'sass', 'scripts', 'fonts']);
+gulp.task('fonts', ['copy-fa', 'ttf2woff']);
+gulp.task('compile', ['pug', 'sass', 'scripts', 'fonts', 'imagemin']);
 gulp.task('build', function(callback) {
   runSequence('cleanBuildDir', 'compile', callback);
 });
-
-gulp.task('default', ['compile', 'browser-sync', 'watch']);
+gulp.task('dev', ['compile', 'browser-sync', 'watch']);
+gulp.task('default', function(callback) {
+  runSequence('cleanBuildDir', 'dev', callback);
+});
